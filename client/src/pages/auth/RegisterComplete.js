9c011852b2c0abb5b7928e773cee5f403e200cf6
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2021-09-09 02:41:23 +0600

    email link verification process

--- client/src/pages/auth/RegisterComplete.js
+++ client/src/pages/auth/RegisterComplete.js
@@ -8,6 +8,8 @@ const RegisterComplete = ({ history }) => {
 
   useEffect(() => {
     setEmail(window.localStorage.getItem("emailForRegistration"));
+    // console.log(window.location.href);
+    // console.log(window.localStorage.getItem("emailForRegistration"));
   }, []);
 
   const handleSubmit = async (e) => {
@@ -15,6 +17,49 @@ const RegisterComplete = ({ history }) => {
     // Prevent browser from auto reload
     // =======================================================================================
     e.preventDefault();
+
+    // =======================================================================================
+    // Validation
+    // =======================================================================================
+    if (!email || !password) {
+      toast.error("Email and Password is required!");
+      return;
+    } else if (password.length < 6) {
+      toast.error("Password must be at least 6 characters long!");
+      return;
+    }
+
+    try {
+      const result = await auth.signInWithEmailLink(
+        email,
+        window.location.href
+      );
+      // console.log(result.user.emailVerified);
+      // console.log(result);
+      if (result.user.emailVerified) {
+        // =======================================================================================
+        // Remove user email from local storage
+        // =======================================================================================
+        window.localStorage.removeItem("emailForRegistration");
+        // =======================================================================================
+        // Get user id token
+        // =======================================================================================
+        let user = auth.currentUser;
+        await user.updatePassword(password);
+        const idTokenResult = await user.getIdTokenResult();
+        // =======================================================================================
+        // Redux Store
+        // =======================================================================================
+        console.log("USER", user, "IdTokenResult", idTokenResult);
+        // =======================================================================================
+        // Redirect
+        // =======================================================================================
+        history.push("/");
+      }
+    } catch (error) {
+      // console.log(error);
+      toast.error(error.message);
+    }
   };
 
   const completeRegistrationForm = () => (
